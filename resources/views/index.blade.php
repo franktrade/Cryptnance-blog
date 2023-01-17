ffdbcbf6d0ae878f8cad1c95ac11fcd921e64df3
franktrade <102446996+franktrade@users.noreply.github.com>
2023-01-17 14:55:17 +0100

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<div class="">
+  <div class="flex text-gray-100 pt-10">
+     <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+        <h1 class="">
+          Do you want to become a profitable trader?
+        </h1>
+        <a href="/blog"  
+        class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase"
+        >
+          Read more
+        </a> 
+     </div>
+  </div>
+</div>
+@endsection
